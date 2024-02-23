c974f7da22c19fa9ad935dc96f16a7b4d0054f71
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2024-02-22 23:56:09 -0500

    boostrapify the websites: show friend, form from edit and new, and view all friends page. Installed devise for user management and generate routes + pages using generator

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  #On host we are suppsoed to put the url from deployment
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
